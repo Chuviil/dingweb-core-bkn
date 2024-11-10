65ee64a3605f0cc20262f542da3a44a036496cad
Sebastian Quito <josue.quito@udla.edu.ec>
2024-11-10 13:27:26 -0500

    Created OpenAI Embeddings module

---
+++ src/openai/openai.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import {OpenAIEmbeddings} from "@langchain/openai";
+
+@Injectable()
+export class OpenaiService extends OpenAIEmbeddings{
+    constructor() {
+        super({model: "text-embedding-3-small"});
+    }
+}
